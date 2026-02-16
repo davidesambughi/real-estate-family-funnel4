3b6ab10eacc5148ac0627512fbc6b03e037621bb
davidesambughi <davidesambughi@gmail.com>
2026-02-16 11:45:06 +0000

    layout.tsx - metadata type corrected as <Promise>

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -19,7 +19,7 @@ export async function generateMetadata({
   params
 }: {
   params: Promise<{ locale: string }>;
-}) {
+}): Promise<Metadata> {
   const { locale } = await params;
   const t = await getTranslations({ locale, namespace: 'Metadata' });
 
